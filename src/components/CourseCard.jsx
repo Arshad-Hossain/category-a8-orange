418079d50946d7b34be0425cb42cbc7297dd29e8
arshadhossainoh <arshadhossain.oh@gmail.com>
2026-04-30 13:35:26 -0400

    worked on popular course section

---
+++ src/components/CourseCard.jsx
@@ -0,0 +1,47 @@
+import { Button, Card, Chip, Separator } from "@heroui/react";
+import Image from "next/image";
+// import Link from "next/link";
+
+const CourseCard = ({ course }) => {
+  return (
+    <Card className="border rounded-xl">
+      <div className="relative w-full aspect-square">
+        <Image
+          src={course.image}
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          alt={course.title}
+          className="object-cover rounded-xl"
+        />
+
+        {/* <Chip size="sm" className="absolute right-2 top-2">
+          {photo.category}
+        </Chip> */}
+      </div>
+
+      <div>
+        <h2 className="font-medium">{course.title}</h2>
+      </div>
+
+      <div className="flex gap-5">
+        <div className="flex items-center gap-2">
+          <p>{course.instructor}</p>
+        </div>
+
+        <Separator orientation="vertical" />
+
+        <div className="flex items-center gap-2">
+          <p>{course.rating}</p>
+        </div>
+      </div>
+
+      {/* <Link href={`/all-photos/${photo.id}`}>
+        <Button variant="outline" className={"w-full"}>
+          View Details
+        </Button>
+      </Link> */}
+    </Card>
+  );
+};
+
+export default CourseCard;
---
+++ src/components/PopularCourses.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import CourseCard from "./CourseCard";
+
+const PopularCourses = async () => {
+  const res = await fetch("https://category-a8-orange.vercel.app/data.json");
+  const courses = await res.json();
+  // console.log(courses, "courses");
+
+  const top_three_courses = courses
+    .sort((a, b) => b.rating - a.rating)
+    .slice(0, 3);
+  console.log(top_three_courses);
+  return (
+    <div>
+      <h1 className="text-xl font-bold my-3">Popular Courses</h1>
+      <div className="grid grid-cols-3 gap-5">
+        {top_three_courses.map((course) => (
+          <CourseCard key={course.id} course={course}></CourseCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PopularCourses;
