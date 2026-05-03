5b81135f4a17af7c932d2d83822c063365c4da4c
arshadhossainoh <arshadhossain.oh@gmail.com>
2026-05-02 21:38:43 -0400

    worked on middleware

--- middleware.js
+++ middleware.js
@@ -3,7 +3,12 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
   const sessionCookie = request.cookies.get("better-auth.session_token");
 
-  if (!sessionCookie && request.nextUrl.pathname.startsWith("/courses")) {
+  if (
+    !sessionCookie &&
+    request.nextUrl.pathname.startsWith(
+      "/https://category-a8-orange.vercel.app/courses",
+    )
+  ) {
     const loginUrl = new URL("/login", request.url);
     loginUrl.searchParams.set("redirect", request.nextUrl.pathname);
     return NextResponse.redirect(loginUrl);
@@ -12,6 +17,6 @@ export function middleware(request) {
   return NextResponse.next();
 }
 
-export const config = {
-  matcher: ["/courses/:path*"],
-};
+// export const config = {
+//   matcher: ["/courses/:path*"],
+// };
