558ddd141ad4c224ff03a6d4de9160a44d07fd84
arshadhossainoh <arshadhossain.oh@gmail.com>
2026-05-03 20:51:29 -0400

    worked on env files

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -2,10 +2,9 @@ import CoursesClient from "@/components/CourseClient";
 import SearchUI from "@/components/SearchUI";
 
 const CoursesPage = async () => {
-  const res = await fetch("https://category-a8-orange.vercel.app/data.json");
-  const courses = await res.json();
-
-  return <CoursesClient courses={courses}></CoursesClient>;
+  // const res = await fetch("https://category-a8-orange.vercel.app/data.json");
+  // const courses = await res.json();
+  // return <CoursesClient courses={courses}></CoursesClient>;
 };
 
 export default CoursesPage;
